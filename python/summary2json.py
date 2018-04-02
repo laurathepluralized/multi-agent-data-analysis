7c81a925015525d6900ae4c7dd11cae24931a7d3
Laura Strickland <laura.strickland@gtri.gatech.edu>
2018-04-02 19:10:58 -0400

    works for some cases, have lvdb breakpoint on a case that somehow only has one line in sample data, will debug soon

--- python/summary2json.py
+++ python/summary2json.py
@@ -13,8 +13,14 @@ def ensure_dir_exists (datadir):
     dir = os.path.dirname(datadir)
     if not os.path.exists(dir):
         os.makedirs(dir)
+            
 
-def glob2json (datadir, already_moved):
+def df_to_json(thedf, thefilename):
+    thedf.reset_index(inplace=True)
+    thedf.to_json(thefilename, orient='index');
+
+def glob2df(datadir, already_moved):
+    print(datadir)
     summary_paths = datadir + '/20*/summary.csv'
     mission_paths = datadir + '/20*/mission.xml'
     # Concatenation procedure from:
@@ -24,16 +30,21 @@ def glob2json (datadir, already_moved):
     df_list = []
 
     for filename, missionname in zip(sorted(data_files), sorted(mission_files)):
-        jobnumpart = filename[26:]
+        jobnumpart = filename.split('_job_',1)[1]
         jobnum = jobnumpart.split('_task',1)[0]
 
         thisjob_df = pd.read_csv(filename)
         # Add column to df for job number
         thisjob_df['job_num']=jobnum
         thisjob_params_df = xml_param_df_cols(missionname);
-        this_job_df = thisjob_df.join(thisjob_params_df);
+        num_lines = len(thisjob_df.index)
+        df_to_append = pd.concat([thisjob_params_df]*num_lines, ignore_index=True);
+        if (df_to_append.empty) or (len(df_to_append) < 2):
+            lvdb.set_trace()
 
-        lvdb.set_trace()
+        # df_to_append['job_num']=jobnum
+
+        this_job_df = pd.concat([thisjob_df, df_to_append], axis=1);
 
         df_list.append(this_job_df)
 
@@ -44,14 +55,31 @@ def glob2json (datadir, already_moved):
     if already_moved:
         thedir = datadir
     else:
-        thedir = datadir + '/job_' + job_num
+        thedir = datadir# + '/job_' + job_num
 
     ensure_dir_exists(thedir)
-    print(thedir)
-    thefilename = thedir + args.filename
-    print(thefilename)
-    return df.to_json(thefilename, orient='index')
+    return df;
 
+
+def append_block(theblock, block_name_string, none_type_var):
+    thedf = pd.DataFrame()
+
+    if type(theblock) is not type(none_type_var):
+        thedict = {}
+        try:
+            thedict = theblock.attrib
+        except:
+            pass
+
+        if thedict:
+            try:
+                thedict[block_name_string] = theblock.text
+            except:
+                print('block has no name:', block_name_string)
+                pass;
+
+            thedf = thedf.from_records([thedict])
+    return thedf;
 
 def xml_param_df_cols(mission_file_name):
     # Borrowing this xml parsing and looping snippet from
@@ -62,94 +90,105 @@ def xml_param_df_cols(mission_file_name):
 
     team_keys = []
     list_df_by_team = []
+    big_df_params = pd.DataFrame()
+
+    # there is probably a nicer way to do this, but this gets the right type
+    notatag = tree.find('this_isnt_a_tag')
+    nonetype = type(notatag)
 
     # Find and loop over all "entity" tags in mission file.
     entity_num = 0
+    key_idx = -1;
     for child in root:
         # This is still a work in progress.
         # Want to get all of the parameters from the various plugins,
         # add the tag name, plugin name, and team number (where applicable) to
         # a Pandas DF column name, and then set the value for that column to be
         # what the param was during simulation as defined by the mission file.
-        if child.tag != "entity":
-                # child.tag != "entity_common":
-                # child.tag != "entity_interaction" \
-                # child.tag != "entity_interaction" \
-                # child.tag != "sensor" \
-                # child.tag != "motion" \
-                # child.tag != "controller" \
-                # child.tag != "metrics":
+        if child.tag != "entity_common" and child.tag != "entity":
             continue
 
-        dummydf = pd.DataFrame
-        notatag = child.find('this_isnt_a_tag')
-        theteam = 0
         df_list_of_attr = []
-        if child.tag == "entity":
-            theteam = child.find('team_id').text
+
+        if child.tag == "entity_common" or child.tag == "entity":
+            ent_common_type = None
+            if child.tag == "entity_common":
+                ent_common_type = child.attrib['name']
+            elif child.tag == "entity":
+                try:
+                    ent_common_type = child.attrib['entity_common']
+                except KeyError:
+                    ent_common_type = None
+
+            theteam = '0'
+            theteamtag = child.find('team_id')
+            if type(theteamtag) != nonetype:
+                # we have a team_id tag
+                theteam = theteamtag.text
+
+            teamint = int(theteam)
+
             entdf = pd.DataFrame
-            team_keys.append(theteam)
+            if (theteam in team_keys):
+                team_keys.append(theteam + 'a')
+            else:
+                team_keys.append(theteam)
+            key_idx += 1;
+
+            toappend = ''
+            if (teamint != 0):
+                toappend += '~t~' + team_keys[key_idx]
+            if ent_common_type is not None:
+                try:
+                    toappend += '~' + ent_common_type
+                except:
+                    lvdb.set_trace()
+
             aut_block = child.find('autonomy')
             motion_block = child.find('motion_model')
             controller_block = child.find('controller')
             sensor_block = child.find('sensor')
 
-            # TODO: make function instead of having same block of code over and 
-            # over
-            if type(aut_block) is not type(notatag):
-                aut_dict = aut_block.attrib
-                autdf = dummydf.from_records([aut_dict])
-                autdf['autonomy'] = child.find('autonomy').text
-                if (entdf.empty):
-                    entdf = autdf
-                else:
-                    entdf = entdf.join(autdf)
+            autdf = append_block(aut_block, 'autonomy', nonetype)
+            if (entdf.empty):
+                entdf = autdf
+            else:
+                entdf = entdf.join(autdf)
 
-            lvdb.set_trace()
+            motiondf = append_block(motion_block, 'motion_model', nonetype)
+            if (entdf.empty):
+                entdf = motiondf
+            else:
+                entdf = entdf.join(motiondf)
 
-            if type(motion_block) is not type(notatag):
-                motion_dict = motion_block.attrib
-                motiondf = dummydf.from_records([motion_dict])
-                motiondf['motion_model'] = child.find('motion_model').text
-                if (entdf.empty):
-                    entdf = motiondf
-                else:
-                    entdf = entdf.join(motiondf)
+            controllerdf = append_block(controller_block, 'controller', nonetype)
+            if (entdf.empty):
+                entdf = controllerdf
+            else:
+                entdf = entdf.join(controllerdf)
 
-            lvdb.set_trace()
+            sensordf = append_block(sensor_block, 'sensor', nonetype)
+            if (entdf.empty):
+                entdf = sensordf
+            else:
+                entdf = entdf.join(sensordf)
 
-            if type(controller_block) is not type(notatag):
-                controller_dict = controller_block.attrib
-                controllerdf = dummydf.from_records([controller_dict])
-                controllerdf['controller'] = child.find('controller').text
-                if (entdf.empty):
-                    entdf = controllerdf
-                else:
-                    entdf = entdf.join(controllerdf)
+            if (not entdf.empty):
+                collist = entdf.columns
+                colmap = {}
+                for col in collist:
+                    colmap[col] = col + toappend
+                entdf.rename(index=str, columns=colmap, inplace=True)
 
-            lvdb.set_trace()
 
-            if type(sensor_block) is not type(notatag):
-                sensor_dict = sensor_block.attrib
-                sensordf = dummydf.from_records([sensor_dict])
-                sensordf['sensor'] = child.find('sensor').text
-                if (entdf.empty):
-                    entdf = sensordf
-                else:
-                    entdf = entdf.join(sensordf)
-
-            lvdb.set_trace()
-
-            # if (theteam != 0):
-            #     entdf.columns = [str(col) + '_' + str(theteam) for col in entdf.columns]
-
-        list_df_by_team.append(entdf)
+        if (big_df_params.empty):
+            big_df_params = entdf
+        else:
+            big_df_params = big_df_params.join(entdf)
 
     # Return a df of all modified attributes of all teams, with team number 
-    # suffixes in column names and with team numbers as keys
-    finaldf = pd.concat(list_df_by_team, keys=team_keys);
-    lvdb.set_trace()
-    return finaldf;
+    # suffixes and/or entity_common attributes in column names
+    return big_df_params;
 
 
 
@@ -159,10 +198,11 @@ def main ():
     parser = argparse.ArgumentParser()
     parser.add_argument('--path', help='Path to root directory containing output directories (dirs containing summary.csv)')
     parser.add_argument('--put_in_given_dir', help='Put generated csv in given directory = true, put generated csv in job_# directory within given directory = false')
+    parser.add_argument('--filename', help='Put generated JSON in file with this name (relative to path argument)(please include extension)')
     args = parser.parse_args()
 
-    thedf = glob2json(args.path, args.put_in_given_dir)
-    print(thedf)
+    thedf = glob2df(args.path, args.put_in_given_dir)
+    df_to_json(thedf, args.path + '/' + args.filename);
 
 
 
