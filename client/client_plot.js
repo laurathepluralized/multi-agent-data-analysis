114d6e8e57f4f43ebb720d1f6052a164b2d6f625
Laura Strickland <laura.strickland@gtri.gatech.edu>
2018-04-03 17:55:02 -0400

    attempting to work from async_flask_2 github repo, but js script wont work when removed from index.html. Somebody who actually knows networking needs to help here, please

--- client/client_plot.js
+++ client/client_plot.js
@@ -1,55 +1,56 @@
 
-
-
-// The following is (loosely) based on the tutorial here:
-// https://www.shanelynn.ie/asynchronous-updates-to-a-webpage-with-flask-and-socket-io/
-// and the github repo here:
+// The following is based on the github repo here:
 // https://github.com/Chainfrog-dev/async_flask_2
 $(document).ready(function() {
     namespace = '/testing';
-    var socket = io.connect('http://' + document.domain + '"' + location.port + '/testing');
-    var received_data = [];
+
+    var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port + namespace);
 
     socket.on('connect', function() {
-        socket.emit('connect_event', {data: 'connected from js side!'});
+        socket.emit('my_event', {data: 'connected from js side!'});
     });
 
-    socket.on('newnum', function(msg) {
-        console.log('Got number ' + msg.number);
+    socket.on('my_response', function(msg) {
+        $('#log').append('<br>' + $('<div>').text('Received #' + msg.counter + ':' + msg.data).html());
 
-        if (received_data.length >= 20) {
-            received_data.shift()
-        }
-        received_data.push(msg.number);
-        num_string = '';
-
-        for (var ii = 0; ii < received_data.length; ii++) {
-            num_string = num_string + '<p>' + received_data[ii].toString() + '</p>';
-        }
-
-        $('#log').html(num_string);
     });
+
+
+    var resp_times = [];
+    var starttime;
+    window.setInterval(function() {
+        starttime = (new Date).getTime();
+        socket.emit('myping');
+    }, 1000);
+
+
+    socket.on('resp', function() {
+        var latency = (new Date).getTime() - starttime;
+        resp_times.push(latency);
+        resp_times = resp_times.slice(-50);
+        var sum = 0;
+        for (var ii = 0; ii < resp_times.length; ii++) {
+            sum += resp_times[ii];
+        };
+
+        $('ping-resp').text(Math.round(10*sum/resp_times.length) / 10);
+    });
+
+    $('form#disconnect').submit(function(event) {
+        socket.emit('disconnect_request');
+        return false;
+    )};
 });
 
 
 
-
-
-
-
-
-
-
-var axPadding = 50;
-var svgSize = {width: 1000, height: 800};
-var margin = {top: 20, right: 5, bottom: 20, left: 100};
-var widthWithLegend = svgSize.width - margin.left - margin.right;
-var width = widthWithLegend - 4*axPadding;
-var heightWithTitle = svgSize.height - margin.top - margin.bottom;
-var height = heightWithTitle - axPadding;
-
-
-
+// var axPadding = 50;
+// var svgSize = {width: 1000, height: 800};
+// var margin = {top: 20, right: 5, bottom: 20, left: 100};
+// var widthWithLegend = svgSize.width - margin.left - margin.right;
+// var width = widthWithLegend - 4*axPadding;
+// var heightWithTitle = svgSize.height - margin.top - margin.bottom;
+// var height = heightWithTitle - axPadding;
 
 // // Based on 
 // // https://stackoverflow.com/questions/14118076/socket-io-server-to-server/24657202#24657202
@@ -88,64 +89,64 @@ var height = heightWithTitle - axPadding;
 // };
 
 
-function tick() {
+//function tick() {
 
 
-//d3.json("wine_quality.csv", function(plotdata) {
+////d3.json("wine_quality.csv", function(plotdata) {
 
-    plotdata.push(
-        plotdata.forEach(function(d) {
-            console.log('this is where intermediary data processing would go')
-        });
+//    plotdata.push(
+//        plotdata.forEach(function(d) {
+//            console.log('this is where intermediary data processing would go')
+//        });
 
-    var xExtent = d3.extent(plotdata, function(d) {
-        return +d.x;
-    });
-    var yExtent = d3.extent(plotdata, function(d) {
-        return +d.y;
-    });
-    var xScale = d3.scale.linear()
-        .domain([d3.min([0, xExtent[0]]), xExtent[1]])
-        .range([0, width]);
+//    var xExtent = d3.extent(plotdata, function(d) {
+//        return +d.x;
+//    });
+//    var yExtent = d3.extent(plotdata, function(d) {
+//        return +d.y;
+//    });
+//    var xScale = d3.scale.linear()
+//        .domain([d3.min([0, xExtent[0]]), xExtent[1]])
+//        .range([0, width]);
 
-    var yScale = d3.scale.linear()
-        .domain([d3.min([0, yExtent[0]]), yExtent[1]])
-        .range([height, 0]);
-
-
-    var xAxis = d3.svg.axis()
-        xAxis.scale(xScale)
-        .orient("bottom");
-
-    var yAxis = d3.svg.axis()
-        yAxis.scale(yScale)
-        .orient("left");
-
-    var svg = d3.select("body").append("svg")
-        .attr("width", svgSize.width)
-        .attr("height", svgSize.height);
-    var svgg = svg.append("g")
-        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-    svgg.append("g")
-        .attr("class", "axis")
-        .attr("transform", "translate(" + 0 + "," + height + ")")
-        .call(xAxis);
-
-    svgg.append("g")
-        .attr("class", "axis")
-        .attr("transform", "translate(" + 0 + "," + 0 + ")")
-        .call(yAxis);
-
-    svgg.selectAll(".point")
-        .data(plotdata)
-      .enter().append("path")
-        .attr("class", "point")
-        .attr("stroke", "black")
-        .attr("transform", function(d) {
-            return "translate(" + (xScale(d.x)) + "," + yScale(d.y) + ")";
-        });
+//    var yScale = d3.scale.linear()
+//        .domain([d3.min([0, yExtent[0]]), yExtent[1]])
+//        .range([height, 0]);
 
 
-}
+//    var xAxis = d3.svg.axis()
+//        xAxis.scale(xScale)
+//        .orient("bottom");
+
+//    var yAxis = d3.svg.axis()
+//        yAxis.scale(yScale)
+//        .orient("left");
+
+//    var svg = d3.select("body").append("svg")
+//        .attr("width", svgSize.width)
+//        .attr("height", svgSize.height);
+//    var svgg = svg.append("g")
+//        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+//    svgg.append("g")
+//        .attr("class", "axis")
+//        .attr("transform", "translate(" + 0 + "," + height + ")")
+//        .call(xAxis);
+
+//    svgg.append("g")
+//        .attr("class", "axis")
+//        .attr("transform", "translate(" + 0 + "," + 0 + ")")
+//        .call(yAxis);
+
+//    svgg.selectAll(".point")
+//        .data(plotdata)
+//      .enter().append("path")
+//        .attr("class", "point")
+//        .attr("stroke", "black")
+//        .attr("transform", function(d) {
+//            return "translate(" + (xScale(d.x)) + "," + yScale(d.y) + ")";
+//        });
+
+
+//}
 
