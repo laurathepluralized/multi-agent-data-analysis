2686425e5a4011966f312264b91b5df20d168abf
Laura Strickland <laura.strickland@gtri.gatech.edu>
2018-04-03 17:55:02 -0400

    attempting to work from async_flask_2 github repo, but js script wont work when removed from index.html. Somebody who actually knows networking needs to help here, please

--- app.py
+++ app.py
@@ -1,61 +1,58 @@
 #!/usr/bin/env python3
-#
-# Basic structure inspired/informed by 
-# https://blog.miguelgrinberg.com/post/easy-websockets-with-flask-and-gevent
+
+from threading import Lock
+from flask import Flask, render_template, session, request
+from flask_socketio import SocketIO, emit, disconnect
+from random import random
+
 
 from python import outfiles2condensed as dat
-from flask import Flask, render_template
-from flask_socketio import SocketIO, emit
-from random import random
-from time import sleep
-from threading import Thread, Event
 
+async_mode = 'eventlet'
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
-socketio = SocketIO(app)
+socketio = SocketIO(app, async_mode=async_mode)
+thread = None
+thread_lock = Lock()
 
-
-thread = Thread()
-thread_stop_event = Event()
-
-class RandomThread(Thread):
-    def __init__(self):
-        self.delay = 1
-        super(RandomThread, self).__init__()
-
-    def rng(self):
-        while not thread_stop_event.isSet():
-            num = round(random()*10, 3)
-            print(num)
-            socketio.emit('newnum', {'number': num}, namespace='/testing')
-            print('emitted number ', num)
-            sleep(self.delay)
-
-    def run(self):
-        self.rng()
-
+def background_thread():
+    count = 0;
+    while True:
+        print('sleeping')
+        socketio.sleep(2)
+        count += 1;
+        number = round(random()*10, 3)
+        socketio.emit('my_response',
+                {'data': number, 'count': count},
+                namespace='/testing')
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', async_mode=socketio.async_mode)
+
 
 @socketio.on('connect', namespace='/testing')
 def test_connect():
     global thread
-    print('client connected')
+    with thread_lock:
+        if thread is None:
+            thread = socketio.start_background_task(target=background_thread)
+    emit('my_response', {'data': 'connected!', 'count': 0})
 
-    if not thread.isAlive():
-        print('starting thread')
-        thread = RandomThread()
-        thread.start()
+@socketio.on('myping', namespace='/testing')
+def ping_resp():
+    emit('resp')
+
+
+
 
 @socketio.on('disconnect', namespace='/testing')
 def test_disconnect():
-    print('client disconnected')
+    print('client disconnected', request.sid)
 
 if __name__ == '__main__':
-    socketio.run(app)
+    socketio.run(app, debug=True, host='0.0.0.0')
 
 
