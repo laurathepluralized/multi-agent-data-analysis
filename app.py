3fa7bb3e68aacf4b9b00e8b9a52d8dd9d3ff5a43
Laura Strickland <laura.strickland@gtri.gatech.edu>
2018-04-03 15:57:25 -0400

    trying to follow a tutorial repo; very buggy work in progress

--- app.py
+++ app.py
@@ -3,20 +3,57 @@
 # Basic structure inspired/informed by 
 # https://blog.miguelgrinberg.com/post/easy-websockets-with-flask-and-gevent
 
+from python import outfiles2condensed as dat
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
+from random import random
+from time import sleep
+from threading import Thread, Event
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
 
+
+thread = Thread()
+thread_stop_event = Event()
+
+class RandomThread(Thread):
+    def __init__(self):
+        self.delay = 1
+        super(RandomThread, self).__init__()
+
+    def rng(self):
+        while not thread_stop_event.isSet():
+            num = round(random()*10, 3)
+            print(num)
+            socketio.emit('newnum', {'number': num}, namespace='/testing')
+            print('emitted number ', num)
+            sleep(self.delay)
+
+    def run(self):
+        self.rng()
+
+
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@socketio.on('got_data')
-def test_message(message):
-    emit('test_response', {'data': message['data']})
+@socketio.on('connect', namespace='/testing')
+def test_connect():
+    global thread
+    print('client connected')
+
+    if not thread.isAlive():
+        print('starting thread')
+        thread = RandomThread()
+        thread.start()
+
+@socketio.on('disconnect', namespace='/testing')
+def test_disconnect():
+    print('client disconnected')
 
 if __name__ == '__main__':
     socketio.run(app)
