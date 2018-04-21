35c9dbd41f6b68652264e58c755c7e768534d790
Hubert Pan <hubert.pan3@gmail.com>
2018-04-21 19:03:46 -0400

    Auth and get code works now

--- expManagerSQLSDKsAndScripts/RDataAccess/dataLoader.R
+++ expManagerSQLSDKsAndScripts/RDataAccess/dataLoader.R
@@ -1,18 +1,23 @@
 library(httr)
 
-getData <- function() {
-
+getData <- function(serverDomain, path, params, authCall) {
+  dataCall <- GET(url = paste(serverDomain, path, sep=""),
+                  add_headers('X-XSRF-TOKEN' = cookies(authCall)[1,'value']),
+                  query = params,
+                  verbose()
+                  )
+  return(dataCall)
 }
 
+# authResp <- authenticate("admin","admin", serverDomain)
 authenticate <- function(username, password, serverDomain) {
     data=list(j_username = username, j_password = password, 'submit' = 'Login')
-     initialCallResponse <- GET(paste(serverDomain, "/#", sep=""), verbose())
+     initialCallResponse <- GET(paste(serverDomain, "/#", sep=""))
      authRes <- POST(
        paste(serverDomain, '/api/authentication', sep=""), 
        add_headers('X-XSRF-TOKEN' = cookies(initialCallResponse)[1,'value']),
        body = data,
-       encode = "form",
-       verbose()
+       encode = "form"
        )
      return(authRes)
 }
