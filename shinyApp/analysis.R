e0f7314cce2c1bd1545b4698e92b4a61bd25ee86
Hubert Pan <hubert.pan3@gmail.com>
2018-04-23 23:22:55 -0400

    add step two of analysis to analysis.R

--- shinyApp/analysis.R
+++ shinyApp/analysis.R
@@ -7,9 +7,56 @@ library(datasets)
 library(corrplot)
 library(data.table)
 library(ggplot2)
+library(randomForest)
 
+library(corrplot)
 
-runStablilityCheck <- function(data, result_col, numericCol, categoryCol) {
+runCorrelation <- function(data, result_col,  numericCols, categoryCols){
+  numericCol <- numericCols
+  categoryCol <- categoryCols
+  
+  ##specify response variable (y) and name it result
+  colnames(data)[colnames(data) == result_col] <- 'result'
+  numericCol <- numericCols
+  categoryCol <- categoryCols
+  
+  ##read useful varaibles only (specified above)
+  mydata <- cbind(data$result, data[,numericCol] , data[,categoryCol])
+  colnames(mydata)[colnames(mydata) == 'data$result'] <- 'result'
+  
+  ##Clean missing data
+  mydata <- na.omit(mydata)
+  mydataOriginal <- mydata
+  
+  ##Drop columns of no information (only one value exists)
+  NumericColToAdd <- c()
+  for(col in numericCol ){
+    if(sd(mydata[,col]) > 0){
+      NumericColToAdd <- cbind(NumericColToAdd, col)
+    }
+  }
+  CatColToAdd <- c()
+  for(col in categoryCol ){
+    temp <- levels(factor(mydata[,col]))
+    if( length(temp[temp != ""]) > 1){
+      CatColToAdd <- cbind(CatColToAdd, col)
+    }
+  }
+  
+  mydata <- cbind(mydata$result, mydata[,NumericColToAdd] , mydata[,CatColToAdd])
+  colnames(mydata) <- cbind("result", NumericColToAdd, CatColToAdd)
+  
+  
+  ####Basic Correlation analysis for numeric varibles
+  Corr <- cor(mydata[,cbind("result", NumericColToAdd)])
+  
+  return(Corr)
+  
+}
+
+runStablilityCheck <- function(data, result_col, numericCols, categoryCols) {
+  numericCol <- numericCols
+  categoryCol <- categoryCols
   colnames(data2)[colnames(data2) == result_col] <- 'result'
   data <- data2
   
@@ -34,4 +81,4 @@ runStablilityCheck <- function(data, result_col, numericCol, categoryCol) {
                            sd   = sd(result),
                            cov   = sd(result) / mean(result)  )
   return (stabilityTable2[order(stabilityTable2$cov),])
-} 
+}
