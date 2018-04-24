4719b3cb1fa54bc392ce6a1c457a999f0945300b
Laura Strickland <laura.strickland@gtri.gatech.edu>
2018-04-24 13:02:40 -0400

    save data as rds, brush data, show 10 rows of brushed data below plot

--- vizplot/app.R
+++ vizplot/app.R
@@ -7,9 +7,15 @@
 library(shiny)
 library(ggplot2)
 
-dsim <- read.csv('./../shinyApp/data/alldata2.csv', stringsAsFactors = FALSE, header=TRUE)
+#options(shiny.maxRequestSize=1000*1024^2)
+dsimcsv <- read.csv('./../shinyApp/data/alldata2.csv', stringsAsFactors = FALSE, header=TRUE)
+storefile <- file.path('./', 'data.rds')
+saveRDS(dsimcsv, file = storefile)
+dsim <- readRDS(storefile)
 
 ui <- fluidPage(
+
+
     selectInput('theparamx', 'Select parameter to plot on x-axis', names(dsim)),
     selectInput('themetricy', 'Select metric to plot on y-axis', names(dsim)),
     selectInput('thefixed', 'Select parameters to fix', names(dsim)),
@@ -18,21 +24,18 @@ ui <- fluidPage(
 )
 
 server <- function(input, output, session) {
-    mydata <- reactive({dsim[, c(input$theparamx, input$themetricy)]})
-    print(mydata)
+
+    mydata <- reactive({
+        dsim[, c(input$theparamx, input$themetricy)]
+    })
   
     output$plot1 <- renderPlot({
         plot(mydata())
     })
 
-    # output$info <- renderText({
-    #     paste0("x=", input$plot_click$x, "\ny=", input$plot_click$y)
-    # })
     output$info <- renderPrint({
         # With base graphics, need to tell it what the x and y variables are.
-        brushedPoints(dsim, input$plot_brush, xvar = input$theparamx, yvar = input$themetricy)
-        # print(input$theparamx)
-        # print(input$themetricy)
+        brushedPoints(dsim, input$plot_brush, xvar = input$theparamx, yvar = input$themetricy)[1:10,]
     })
 
 }
