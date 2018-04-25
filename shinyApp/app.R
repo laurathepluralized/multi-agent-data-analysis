8bfc6f976a5f7123028d66e31d2e23aeb55526f3
Hubert Pan <hubert.pan3@gmail.com>
2018-04-25 19:02:21 -0400

    partial integration of loading.R

--- shinyApp/app.R
+++ shinyApp/app.R
@@ -16,10 +16,11 @@ library(ggplot2)
 library(shinydashboard)
 library(scatterD3)
 source("modeling.R")
+source("analysis.R")
+source('loading.R')
 
 # Read CSV into R
 dsim <- read.csv(file="data/betterdata.csv", header=TRUE, sep=",")
-#dsim <- read.csv('data/betterdata.csv', stringsAsFactors = FALSE, header=TRUE)
 paramcols <- c('turn_rate_max_t_1','vel_max_predator','allow_prey_switching_t_2_predator')
 metriccols <- c('NonTeamCapture')
 
@@ -67,84 +68,7 @@ ui <- dashboardPage(
   dashboardBody(
     tabItems(
       # First tab content
-      tabItem(tabName = "main",
-        fluidRow(
-          box(plotOutput("plot1", height = 250)),
-          
-          box(
-            title = "Controls",
-            sliderInput("slider", "Number of observations:", 1, 100, 50)
-          )
-        ),
-        fluidRow(
-          box (
-            # Input: Select a file ----
-            fileInput("file1", "Choose CSV File",
-              multiple = TRUE,
-              accept = c("text/csv",
-                "text/comma-separated-values,text/plain",
-                ".csv")
-            ),
-          
-            # These column selectors are dynamically created when the
-            # file is loaded
-            uiOutput("fromCol"),
-            uiOutput("toCol"),
-            uiOutput("amountflag"),
-            #The conditional panel is triggered by the preceding checkbox
-            conditionalPanel(
-              condition="input.amountflag==true",
-              uiOutput("amountCol")
-            ),
-            
-            # Horizontal line ----
-            tags$hr(),
-            
-            # Input: Checkbox if file has header ----
-            checkboxInput("header", "Header", TRUE),
-            
-            # Input: Select separator ----
-            radioButtons("sep", "Separator",
-              choices = c(Comma = ",",
-                Semicolon = ";",
-                Tab = "\t"),
-              selected = ","),
-            
-            # Input: Select quotes ----
-            radioButtons("quote", "Quote",
-              choices = c(None = "",
-                "Double Quote" = '"',
-                "Single Quote" = "'"),
-              selected = '"'),
-            
-            # Horizontal line ----
-            tags$hr(),
-            
-            # Input: Select number of rows to display ----
-            radioButtons("disp", "Display",
-              choices = c(Head = "head",
-                All = "all"),
-              selected = "head"),
-            checkboxGroupInput("inCheckboxGroup",
-              "Checkbox group input:",
-              c("label 1" = "option1",
-              "label 2" = "option2")),
-            uiOutput("choose_columns")
-            
-                      
-          ),
-          box(
-            tableOutput("contents")
-          )
-        ),
-        fluidRow(
-          box(
-            title = "Stability Analysis",
-            tableOutput("stabilityAnalysis"),
-            width = 12
-          )
-          
-        )
+      tabItem(tabName = "main", loading_ui()
       ),
       # Second tab content
       tabItem(tabName = "widgets",
