4719f5e8bac49ba31e568f85043d6a37f25d3ff3
Laura Strickland <laura.strickland@gtri.gatech.edu>
2018-04-28 11:53:35 -0400

    latest outfilestocondensed, updated clean_data for current headers

--- pythonStuff/clean_data.py
+++ pythonStuff/clean_data.py
@@ -27,30 +27,19 @@ def clean(filename):
 
     #  Unimportant columns
     df.drop(columns=[ \
-            "autonomy_t_1", \
-            "autonomy_t_2_predator", \
             "autonomy_t_3_predator", \
-            "motion_model_predator", \
-            "motion_model_t_1", \
-            # "results_dir",  # actually want this one so we can show where the raw data is
             "entity_count", \
             "flight_time", \
             "flight_time_norm", \
             "non_team_coll", \
             "team_coll", \
             "ground_coll", \
-            # Score is a duplicate of NonTeamCaptures, our output metric
-            "score", \
-            # TeamCaptures is irrelevant for this simulation
-            "TeamCaptures", \
-            # Other things to remove
             "max_pred_speed_t_1", \
             "align_weight_t_2_predator", \
             "avoid_nonteam_weight_t_2_predator", \
             "avoid_team_weight_t_2_predator", \
             "centroid_weight_t_2_predator", \
             "comms_range_t_2_predator", \
-            "max_speed_t_2_predator", \
             "align_weight_t_3_predator", \
             "avoid_nonteam_weight_t_3_predator", \
             "avoid_team_weight_t_3_predator", \
@@ -59,11 +48,6 @@ def clean(filename):
             "max_speed_t_3_predator" \
             ], \
             inplace=True, errors='ignore')
-    df = df.drop([0, 1]).reset_index(drop=True)
-        
-        
-    #Theoretically this data is more ready now
-    df = df[pd.notnull(df["NonTeamCapture"])]
 
     #File to write to
     df.to_csv(filename + "_clean.csv", index=0)
@@ -76,8 +60,6 @@ def main():
     args = parser.parse_args()
 
     clean(args.path)
-
-
 
 
 if __name__ == '__main__':
--- pythonStuff/outfiles2condensed.py
+++ pythonStuff/outfiles2condensed.py
@@ -1,6 +1,9 @@
 import os
+import shutil
 #import re
 import sys
+import platform
+import subprocess
 import numpy as np
 import json
 import pickle
@@ -9,12 +12,35 @@ from pandas import Series
 import xml.etree.ElementTree as ET
 import glob
 import argparse
-import pdb
+try:
+    import lvdb
+except:
+    import pdb as lvdb
+    print('using pdb instead of lvdb')
+    pass
 
 def ensure_dir_exists (datadir):
-    dir = os.path.dirname(datadir)
-    if not os.path.exists(dir):
-        os.makedirs(dir)
+    if not os.path.exists(datadir):
+        os.makedirs(datadir)
+    if not os.path.exists(datadir):
+        themessage = 'Directory {} could not be created.'.format(datadir)
+        if (int(platform.python_version()[0]) > 2):
+            raise NotADirectoryError(themessage)
+        else:
+        # python 2 doesn't have the impressive exception vocabulary 3 does
+        # so just raising a generic exception with a useful description
+            raise BaseException(themessage)
+
+def rsync_the_file (from_location, to_location):
+    # Assuming that the responses for how platform.system() responds to
+    # different OSes given here are correct (though not assuming case):
+    # https://stackoverflow.com/questions/1854/python-what-os-am-i-running-on
+    if platform.system().lower() is 'windows':
+        print('Windows detected. The rsync command that is about to be', \
+                'executed assumes a Linux or Mac OS; no guarantee that it', \
+                'will work with Windows. Please be ready to transfer files', \
+                'via alternate means if necessary.')
+    subprocess.call(['rsync', '-vaPhz', from_location, to_location])
 
 def df_to_pickle(thedf, thefilename):
     thedf.to_pickle(thefilename);
@@ -25,14 +51,16 @@ def df_to_csv(thedf, thefilename):
 def df_to_json(thedf, thefilename):
     thedf.to_json(thefilename, orient='records', double_precision = 10, force_ascii = True);
 
-def glob2df(datadir, linecount):
+def glob2df(datadir, linecount, jobnum_list):
     print(datadir)
     thepaths = glob.iglob(datadir + '/*/')
+
+    results_dirs_used = []
 
     df_list = []
     progress_counter = 1000;
     counter = 0;
-    # for filename, missionname in zip(sorted(data_files), sorted(mission_files)):
+
     for dirname in sorted(thepaths):
         dirstructure = dirname.split('/')
         lastdir = dirstructure[-1]
@@ -63,11 +91,15 @@ def glob2df(datadir, linecount):
             print('No mission file at ', missionname);
             continue;
 
-
         split_on_task = lastdir.split('_task_')
         tasknum = int(split_on_task[-1])
 
         jobnum = int(split_on_task[0].split('_job_',1)[1])
+
+        if jobnum_list and jobnum not in jobnum_list:
+            # lvdb.set_trace()
+            # print('Job {} not in list of jobs; skipping'.format(jobnum))
+            continue;
 
         counter += 1;
         if counter > progress_counter:
@@ -106,6 +138,7 @@ def glob2df(datadir, linecount):
         this_job_df = thisjob_df
         if not df_to_append.empty:
             this_job_df = pd.concat([thisjob_df, df_to_append], axis=1);
+            results_dirs_used.append(dirname);
 
         # indexed_by_team_df = this_job_df.set_index(['team_id'])
         # df_list.append(indexed_by_team_df)
@@ -115,7 +148,7 @@ def glob2df(datadir, linecount):
 
     print('df created for job ', jobnum)
 
-    return df;
+    return df, results_dirs_used;
 
 
 def append_block(theblock, blk_name, nonetype_var):
@@ -249,7 +282,8 @@ def xml_param_df_cols(mission_file_name):
 
 
 def main ():
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(
+            formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     parser.add_argument('--path', help='Path to root directory containing' \
             ' output directories (dirs containing summary.csv and mission.xml)')
     parser.add_argument('--filename', help='Put generated CSV and/or JSON in' \
@@ -266,42 +300,126 @@ def main ():
             'existing pickle of data (at path/filename.pickle) and generate a'
             ' new one. Choose this option if the data at path has changed.',
             action='store_true')
+    parser.add_argument('-d', '--dest', help='Destination to which to rsync' \
+            ' the aggregated file and pickle to (assumes ssh keys are set' \
+            ' up). NOTE: This rsync method is not guaranteed to work on' \
+            ' Windows!')
+    parser.add_argument('--move_dirs', help='If this flag is set,' \
+            ' results directories will be moved to same location as' \
+            ' aggregated results.', action='store_true')
+    parser.add_argument('--start_job', help='Starting job number')
+    parser.add_argument('--end_job', help='Ending job number')
+    parser.add_argument('--job_list', nargs='+', help='List of job numbers')
     args = parser.parse_args()
+
+    movedirs = args.move_dirs
+    repickle = args.repickle
+
+    # If we want to move results directories anyway, we will need to run
+    # glob2df to get directory list, so need to regenerate pickle rather
+    # than reading from one that is already there that won't have all
+    # the info we need
+    if movedirs:
+        repickle = True
 
     thedf = pd.DataFrame();
 
-    picklename = args.path + '/' + args.filename + '.pickle'
+    extensionless_path = args.path + '/' + args.filename
+
+    startjob = -1
+    endjob = 0
+    use_jobnums = False;
+
+    joblist = [];
+
+    if args.job_list:
+        startjob=int(args.job_list[0])
+        endjob=int(args.job_list[-1])
+        joblist = args.job_list
+        use_jobnums = True;
+    elif args.start_job and args.end_job:
+        startjob = int(args.start_job)
+        endjob = int(args.end_job)
+        use_jobnums = True;
+        # adding 1 to endjob so range will include the end job in the list
+        joblist_temp = range(startjob, endjob+1)
+        if (int(platform.python_version()[0]) > 2):
+            joblist = list(joblist_temp)
+        else:
+            joblist = joblist_temp
+
+    jobdirname = args.path + '/jobs_all'
+    if use_jobnums:
+        jobdirname = args.path + '/jobs_' + str(startjob) + '_' + str(endjob)
+
+    ensure_dir_exists(jobdirname)
+    print('Aggregated data will be placed into {}'.format(jobdirname))
+
+    extensionless_path = jobdirname + '/' + args.filename
+    picklename = extensionless_path + '.pickle'
 
     linecount = args.linecount;
 
-    # generate pickle if none exists or if args.repickle is set to true
-    if not args.repickle:
+    made_pickle = False;
+    made_json = False;
+    made_csv = False;
+
+    results_dirs_list = []
+
+    # generate pickle if none exists or if repickle is set to true
+    if not repickle:
         print('Looking for pickle file ', picklename)
         try:
             thedf = pd.read_pickle(picklename)
             print('Loaded pickle file ', picklename)
+            made_pickle = True;
         except:
             print('No pickle found or error reading pickle. Generating new' \
             ' pickle.')
-            thedf = glob2df(args.path, linecount)
+            thedf, results_dirs_list = glob2df(args.path, linecount, joblist)
             thedf.reset_index(inplace=True, drop=True)
             df_to_pickle(thedf, picklename);
+            made_pickle = True;
     else:
         print('Regenerating pickle file at ', picklename)
         # argument explicitly given to regenerate the pickle file
-        thedf = glob2df(args.path, linecount)
+        thedf, results_dirs_list = glob2df(args.path, linecount, joblist)
         thedf.reset_index(inplace=True, drop=True)
         df_to_pickle(thedf, picklename);
+        made_pickle = True;
+
+
 
     # Generate json
     if args.json:
-        jsonname = args.path + '/' + args.filename + '.json'
+        jsonname = extensionless_path + '.json'
         df_to_json(thedf, jsonname);
+        made_json = True;
 
     # generate csv
     if args.csv:
-        csvname = args.path + '/' + args.filename + '.csv'
+        csvname = extensionless_path + '.csv'
         df_to_csv(thedf, csvname);
+        made_csv = True;
+
+    if args.dest:
+        # not sure why wildcards not working here; path + '.*' gives error
+        if made_pickle:
+            rsync_the_file(extensionless_path + '.pickle', args.dest)
+        if made_csv:
+            rsync_the_file(extensionless_path + '.csv', args.dest)
+        if made_json:
+            rsync_the_file(extensionless_path + '.json', args.dest)
+
+    if movedirs:
+        if results_dirs_list:
+            if jobdirname is not None:
+                for dirname in results_dirs_list:
+                    shutil.move(dirname, jobdirname)
+            else:
+                print('No dest; results dir {} not moved'.format(dirname))
+
+
 
 if __name__ == '__main__':
     sys.exit(main())
